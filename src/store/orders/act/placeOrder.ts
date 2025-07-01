425c4f09a9dd1996d6e0b84bf09f69ec54005544
Marwan878 <marwanmohammad432@gmail.com>
2025-07-01 11:09:17 +0300

    Fix placeOrder function

--- src/store/orders/act/placeOrder.ts
+++ src/store/orders/act/placeOrder.ts
@@ -13,50 +13,54 @@ export const placeOrder = createAsyncThunk(
       cart: { products },
     } = getState() as RootState;
 
-    let subtotal = 0;
-
-    products.forEach(async (product) => {
-      const { data: optionCombinations, error } = (await supabase
-        .from("products")
-        .select("optionCombinations")
-        .eq("id", product.product_id)) as unknown as {
-        data: TProductOptionCombination[] | null;
-        error: PostgrestError | null;
-      };
-
-      if (!optionCombinations) throw new Error("Product not found");
-
-      if (error) throw new Error(error.message);
-
-      const currentCombination = optionCombinations.find(
-        (comination) => comination.sku === product.sku
-      );
-
-      if (!currentCombination) throw new Error("Product not found");
-
-      if (currentCombination.stock < product.quantity) {
-        throw new Error(
-          `Not enough stock, please reduce the quantity of ${product.product.name} to ${currentCombination.stock}`
-        );
-      }
-
-      subtotal += product.quantity * currentCombination.price.discounted;
-
-      currentCombination.stock -= product.quantity;
-
-      const { error: updateError } = await supabase
-        .from("products")
-        .update({ optionCombinations })
-        .eq("id", product.product_id);
-
-      if (updateError) throw new Error(updateError.message);
-    });
-
     try {
       const {
         data: { session },
       } = await supabase.auth.getSession();
       if (!session) throw new Error("You must be logged in to place an order");
+
+      let subtotal = 0;
+      products.forEach(async (product) => {
+        const {
+          data: { optionCombinations },
+          error,
+        } = (await supabase
+          .from("products")
+          .select("optionCombinations")
+          .eq("id", product.product_id)
+          .single()) as unknown as {
+          data: { optionCombinations: TProductOptionCombination[] };
+          error: PostgrestError | null;
+        };
+
+        if (!optionCombinations) throw new Error("Product not found");
+
+        if (error) throw new Error(error.message);
+
+        const currentCombination = optionCombinations.find(
+          (comination) => comination.sku === product.sku
+        );
+
+        if (!currentCombination)
+          throw new Error("Product combination not found");
+
+        if (currentCombination.stock < product.quantity) {
+          throw new Error(
+            `Not enough stock, please reduce the quantity of ${product.product.name} to ${currentCombination.stock}`
+          );
+        }
+
+        subtotal += product.quantity * currentCombination.price.discounted;
+
+        currentCombination.stock -= product.quantity;
+
+        const { error: updateError } = await supabase
+          .from("products")
+          .update({ optionCombinations })
+          .eq("id", product.product_id);
+
+        if (updateError) throw new Error(updateError.message);
+      });
 
       const { data, error } = await supabase.functions.invoke(
         "create-checkout-session",
